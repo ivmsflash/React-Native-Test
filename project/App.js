8f6ad6ecfd31eeba05baf4a9dc7ac21a90911ac2
ivmsflash <vlasovms@gmail.com>
2017-11-01 01:45:05 +0300

    Build Page 2

--- project/App.js
+++ project/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, Alert, ScrollView } from 'react-native';
 import TimerMixin from 'react-timer-mixin';
+import { List, ListItem, Button, FormLabel, FormInput } from 'react-native-elements'
+
+const list = [];
+for(let i=1;i<=15;i++)
+{
+  list.push({
+    title: "Элемент "+i,
+    subtitle: "Краткое описание "+i,
+    price: "$"+i
+  });
+};
 
 export default class App extends React.Component {
   componentDidMount() {
@@ -13,52 +24,64 @@ export default class App extends React.Component {
   }
 
   render() {
-    if (!this.state){
+    if (!this.state)
+    {
+      let pic = {
+        uri: 'https://avatars1.githubusercontent.com/u/9402725?s=400&u=32892caa0c777b48247e5156883ba92bf4f938f4&v=4'
+      };
       return (
         <View style={styles.container}>
-          <Page1/>
+          <Image source={pic} style={{width: 100, height: 100, alignItems: 'center'}}/>
         </View>
       );
-    }else{
+    }
+    else
+    {
       return (
-        <View style={styles.page2}>
-          <Page2/>
-        </View>
+        <Page2/>
       );
     }
   }
 }
 
-class Page1 extends React.Component {
-  render() {
-    let pic = {
-      uri: 'https://avatars1.githubusercontent.com/u/9402725?s=400&u=32892caa0c777b48247e5156883ba92bf4f938f4&v=4'
-    };
-    return (
-      <Image source={pic} style={{width: 100, height: 100, alignItems: 'center'}}/>
-    );
-  }
-}
-
 class Page2 extends React.Component {
-  render() {
-    return (
-      <View>
-        <Page2Item title="Заголовок 1" subtitle="Подзаголовок 1" price="1$"/>
-        <Page2Item title="Заголовок 2" subtitle="Подзаголовок 2" price="1$"/>
-        <Page2Item title="Заголовок 3" subtitle="Подзаголовок 3" price="1$"/>
-      </View>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPage3: false,
+      data: null
+    }
   }
-}
-
-class Page2Item extends React.Component {
   render() {
+    const onSelectItem = (itm) => {
+      this.state.showPage3 = true;
+      this.state.data = itm;
+    };
+
+    if (this.state.showPage3) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <Page3 />
+        </View>
+      );
+    }
+
     return (
-      <View>
-        <Text style={styles.page2itemTitle}>{this.props.title}</Text>
-        <Text style={styles.page2itemSubTitle}>{this.props.subtitle}</Text>
-      </View>
+      <ScrollView>
+        <List containerStyle={{marginBottom: 20}}>
+          {
+            list.map((l, i) => (
+              <ListItem
+                title={l.title}
+                key={i}
+                subtitle={l.subtitle}
+                rightTitle={l.price}
+                onPress={() => {onSelectItem(l)}}
+              />
+            ))
+          }
+        </List>
+      </ScrollView>
     );
   }
 }
@@ -66,8 +89,9 @@ class Page2Item extends React.Component {
 class Page3 extends React.Component {
   render() {
     return (
-      <View style={{flex: 1, backgroundColor: 'powderblue'}}>
-        
+      <View>
+        <FormLabel>Ваш E-mail</FormLabel>
+        <FormInput/>
       </View>
     );
   }
@@ -79,20 +103,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  page2: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-  },
-  page2itemTitle: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  page2itemSubTitle: {
-    color: 'red',
-    fontSize: 20,
-  },
+  }
 });
